5ff80702f4960aec6592e17eb388b73d4fbb0a42
= <=>
2019-11-19 10:18:26 +0700

    pesan kalian

---
+++ precache-manifest.7f9cefce493008df84f57b3eff66ccb8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ecbebe01d3c402fa98b",
+    "url": "/css/chunk-vendors.f075aff1.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "5370861dfdd39515ea423238e83008c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "425717f0ff2e5d4ebf73",
+    "url": "/js/about.2dccda56.js"
+  },
+  {
+    "revision": "510d846e96bab8196bc6",
+    "url": "/js/app.b9d4c300.js"
+  },
+  {
+    "revision": "8ecbebe01d3c402fa98b",
+    "url": "/js/chunk-vendors.169b2f68.js"
+  },
+  {
+    "revision": "cc8dc29ec9fe406d34c945151e11aa39",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
